ab18047df28940be81b896cc8cb648a4ecc4c73e
fhui28 <fhui28@gmail.com>
2024-01-16 16:17:33 +1100

    Fix problems with manylm when there are weights passed in

--- R/manylm.R
+++ R/manylm.R
@@ -142,10 +142,12 @@ if (is.empty.model(mt)) {
           abundances  <- abundances[ok,, drop=FALSE]
           X           <- X[ok,, drop=FALSE]
        } 
+       
        if (NCOL(w) == 1) { 
           z <- manylm.wfit(x=X, y=abundances, w=w, offset = offset, 
                             singular.ok = singular.ok, ... ) 
-        } else { 
+        } 
+       if(NCOL(w) > 1) { 
           z <- manylm.multiwfit(x=X, y=abundances, w=w, offset = offset, 
                                     singular.ok = singular.ok, ... )
         }
@@ -224,7 +226,7 @@ return(z)
 ############################################################################
 ## manylm.fit                                                              #
 ############################################################################
-manylm.fit <- function(x, y, offset = NULL, tol=1.0e-010, singular.ok = TRUE, num.factors=NULL, ...) 
+manylm.fit <- function(x, y, offset = NULL, tol=1.0e-010, singular.ok = TRUE, ...) 
 {
     y <- as.matrix( unabund(y) )
     if (is.null(n <- nrow(x)))
@@ -235,47 +237,47 @@ manylm.fit <- function(x, y, offset = NULL, tol=1.0e-010, singular.ok = TRUE, nu
     ny <- ncol(y)
     if (p == 0) {
         return(list(coefficients = numeric(0), residuals = y,
-            fitted.values = 0 * y, rank = 0, df.residual = NROW(y)))
+                    fitted.values = 0 * y, rank = 0, df.residual = NROW(y)))
     }
-
+    
     if (!is.null(offset)) y<- y - offset
     if (nrow(y) != n)
         stop("dimensions of 'x' and 'y' do not match")
-
+    
     if (length(list(...))){
-	  tmp <- paste(names(list(...)), sep = ", ")
+        tmp <- paste(names(list(...)), sep = ", ")
         warning("extra arguments ", tmp ,
-            " are just disregarded.")
+                " are just disregarded.")
     }
     storage.mode(x) <- "double"
     storage.mode(y) <- "double"
-
+    
     ############ BEGIN Fit multivariate linear model  #######
     results     <- list( )
     results$assign    <- attr(x, "assign")  
     qrx         <- qr(x)
     rank        <- qrx$rank
     p1          <- 1:rank
-
+    
     if (rank < p & !singular.ok ) 
-       stop("singular fit encountered") 
+        stop("singular fit encountered") 
     x.ind    <- x[,qrx$pivot[p1], drop=FALSE]
     xnames   <- dimnames(x)[[2]]
     if (is.null(xnames))
-       xnames <- paste("x", 1:p, sep = "")
+        xnames <- paste("x", 1:p, sep = "")
     colnames(qrx$qr) <- xnames[qrx$pivot]
-
+    
     coefficients <- matrix(nrow=p,ncol=ny)
     coefficients[qrx$pivot[p1],] <- chol2inv(qrx$qr[p1,p1, drop=FALSE])%*%t(x.ind)%*% y
-
+    
     dimnames(coefficients) <- list( xnames,dimnames(y)[[2]] )
     hat.X <- x.ind %*% chol2inv(qrx$qr[p1,p1, drop=FALSE]) %*% t(x.ind)
     fitted.values <- x.ind %*% chol2inv(qrx$qr[p1,p1, drop=FALSE])%*% t(x.ind)%*% y
-     # less efficient than x%*%coefficients, but more stable.
+    # less efficient than x%*%coefficients, but more stable.
     residuals     <- y - fitted.values
     colnames(residuals) <- colnames(y)
     df.residual   <- n - rank
-
+    
     ########### END Fit multivariate linear model ############
     results$coefficients        <- coefficients
     results$residuals           <- residuals
@@ -286,133 +288,92 @@ manylm.fit <- function(x, y, offset = NULL, tol=1.0e-010, singular.ok = TRUE, nu
     results$hat.X               <- hat.X  
     results$txX                 <- t(x.ind)%*% x.ind
     results$data                <- data
-
+    
     # To add: results$effects
     # (for not null fits) 'n' vector of orthogonal single-df effects.
     # The first 'rank' of them correspond to non-aliased
     # coeffcients, and are named accordingly.
     return(results)
 }
-                
-                
+
+
 ################################################################################
 ## manylm.wfit                                                             #
 ################################################################################
 
 manylm.wfit<-function(x, y, w, offset = NULL, tol=1.0e-010,  singular.ok = TRUE, ...)
 {
-   y <- as.matrix(unabund(y))
-   if (is.null(n <- nrow(x)))
-      stop("'x' must be a matrix")
-   if (n == 0)
-      stop("0 (non-NA) cases in 'x'")
-   p <- ncol(x)
-   if (p == 0) {
-      return(list(coefficients = numeric(0), residuals = y,
-          fitted.values = 0 * y, rank = 0, df.residual = NROW(y)))
+    y <- as.matrix(unabund(y))
+    if (is.null(n <- nrow(x)))
+        stop("'x' must be a matrix")
+    if (n == 0)
+        stop("0 (non-NA) cases in 'x'")
+    p <- ncol(x)
+    if (p == 0) {
+        return(list(coefficients = numeric(0), residuals = y,
+                    fitted.values = 0 * y, rank = 0, df.residual = NROW(y)))
     }
     ny <- ncol(y)
-
+    
     if (!is.null(offset)) y <- y - offset
     if (nrow(y) != n)
         stop("dimensions of 'x' and 'y' do not match")
-
+    
     if (length(list(...))){
-	  tmp <- paste(names(list(...)), sep = ", ")
+        tmp <- paste(names(list(...)), sep = ", ")
         warning("extra arguments ", tmp ,
-            " are just disregarded.")
+                " are just disregarded.")
     }	
     storage.mode(x) <- "double"
     storage.mode(y) <- "double"
-
+    
     n.ok    <- n - sum(w == 0)
     if(n.ok==0)
         stop("No informative observations. All 'weights' are equal to Zero.")
-
-    pivot <- qrx$pivot
-
+    
     ############# BEGIN Fit multivariate linear model  ##################
     results     <- list()
     results$assign <- attr(x, "assign")
     xnames      <- colnames(x)
     if (is.null(xnames))
         xnames <- paste("x", 1:p, sep = "")
-
+    
     ok    <- w != 0
     qrx   <- qr(x[ok,,drop=FALSE] * sqrt(w[ok]))  # Exclude cases with weights=0.
     colnames(qrx$qr) <- xnames[qrx$pivot]
-
+    
     rank  <- qrx$rank
     p1    <- 1:rank
-
+    
     if (rank < p & !singular.ok ) 
-      stop("singular fit encountered") 
-
+        stop("singular fit encountered") 
+    
     x.ind <- x[ok,qrx$pivot[p1], drop=FALSE] 
-
+    
     coefficients  <- matrix(nrow=p,ncol=ny)
     coefficients[qrx$pivot[p1],]    <- 
-    chol2inv(qrx$qr[p1,p1, drop=FALSE])%*%t(x.ind)%*% (y*w)[ok,,drop=FALSE]
+        chol2inv(qrx$qr[p1,p1, drop=FALSE])%*%t(x.ind)%*% (y*w)[ok,,drop=FALSE]
     dimnames(coefficients) <- list(xnames,dimnames(y)[[2]])
     hat.X  <- x.ind %*% chol2inv(qrx$qr[p1,p1, drop=FALSE])%*% t(x.ind*w[ok])
-
+    
     fitted.values <- x %*% ( chol2inv(qrx$qr[p1,p1, drop=FALSE])%*% t(x.ind) %*%
-    (y*w)[ok,,drop=FALSE] ) # less efficient than x%*%coefficients, but more stable.
-
+                                 (y*w)[ok,,drop=FALSE] ) # less efficient than x%*%coefficients, but more stable.
+    
     residuals      <- y-fitted.values
     # Ensure numerical stability for weights=0 cases.
     residuals[!ok] <- y[!ok]
     colnames(residuals) <- colnames(y)
-
+    
     df.residual   <- n.ok-rank
     ############## END Fit multivariate linear model ################
-
+    
     results$coefficients    <- coefficients
     results$residuals       <- residuals
     results$fitted.values   <- fitted.values
     results$weights         <- w
     results$rank            <- rank
-    results$qr              <- qrx # QR decomposition of x * sqrt(w)if (rank < p & !singular.ok ) 
-      stop("singular fit encountered") 
-  
-    x.ind <- x[,pivot[p1], drop=FALSE]
-
-    coefficients  <- matrix(nrow=p,ncol=ny)
-    txy      <-  t( x.ind ) %*% ( y*w )
-    chol2invqrx  <-  list()
-    hat.X  <- txX <- list()
-
-for( i in 1:(ncol(w)) ) {
-   qrx[[i]]         <- qr( x * sqrt(w[,i]) )
-   chol2invqrx[[i]] <- chol2inv( qrx[[i]]$qr[p1,p1, drop=FALSE] )
-   coefficients[ pivot[p1], ] <-  chol2invqrx[[i]] %*% txy
-   txX[[i]] <- t(x.ind*w[,i]) %*% x.ind
-}
-
-dimnames(coefficients) <- list(xnames,dimnames(y)[[2]])
-
-
-fitted.values <- x %*% coefficients
-
-residuals     <- y - fitted.values
-# Ensure the numerical stability for weights=0 cases.
-residuals[ok] <- y[ok]
-colnames(residuals) <- colnames(y)
-
-df.residual   <- n.ok - rank
-
-################## END Fit multivariate linear model ##################
-
-results$coefficients        <- coefficients
-results$residuals           <- residuals
-results$fitted.values       <- fitted.values
-results$weights             <- w
-results$rank                <- rank
-results$qr                  <- qrx # QR decomposition of x * sqrt(w)
-results$df.residual         <- df.residual
-
-return(results)
-    results$df.residual     <- df.residual
+    results$qr              <- qrx # QR decomposition of x * sqrt(w)
+    results$df.residual         <- df.residual
     results$hat.X           <- hat.X # weighted, cases with weights=0 are not included
     results$txX             <- t(x.ind*w[ok])%*% x.ind  # weighted
     # To add: results$effects
@@ -420,7 +381,7 @@ return(results)
     # The first 'rank' of them correspond to non-aliased
     # coeffcients, and are named accordingly.
     return(results)
-
+    
 }
 
 ################################################################################
@@ -428,97 +389,97 @@ return(results)
 ################################################################################
 manylm.multiwfit <- function(x, y, w, offset = NULL, tol=1.0e-010, singular.ok = TRUE, ...)
 {
-   y <- as.matrix(unabund(y))
-   w <- as.matrix(w)
-   if (is.null(n <- nrow(x)))
+    y <- as.matrix(unabund(y))
+    w <- as.matrix(w)
+    if (is.null(n <- nrow(x)))
         stop("'x' must be a matrix")
-   if (n == 0)
+    if (n == 0)
         stop("0 (non-NA) cases in 'x'")
-   p <- ncol(x)
- 
-   if (p == 0) {
-      return(list(coefficients = numeric(0), residuals = y,
-          fitted.values = 0 * y, rank = 0, df.residual = NROW(y)))
+    p <- ncol(x)
+    
+    if (p == 0) {
+        return(list(coefficients = numeric(0), residuals = y,
+                    fitted.values = 0 * y, rank = 0, df.residual = NROW(y)))
     }
     
     ny <- ncol(y)
-
+    
     if (!is.null(offset)) y <- y - offset
     if (nrow(y) != n)
         stop("dimensions of 'x' and 'y' do not match")
-
+    
     if( ncol(y) != ncol(w) )  stop("dimensions of 'y' and 'w' do not match")
-
+    
     if (length(list(...))){
-	  tmp <- paste(names(list(...)), sep = ", ")
+        tmp <- paste(names(list(...)), sep = ", ")
         warning("extra arguments ", tmp ,
-            " are just disregarded.")
-	}
-
+                " are just disregarded.")
+    }
+    
     storage.mode(x) <- "double"
     storage.mode(y) <- "double"
-
+    
     ok      <- w == 0
     n.ok    <- n - colSums( w == 0 )
     if( any(n.ok == 0) )
-      stop("No informative observations in the weights for variable ",
-    which(n.ok==0), ". All 'weights' are equal to Zero.")
+        stop("No informative observations in the weights for variable ",
+             which(n.ok==0), ". All 'weights' are equal to Zero.")
     
     ################### BEGIN Fit multivariate linear model  ##################
     ####### does the same as model.fit, but I can return txX and hat.X ########
-
+    
     results         <- list()
     results$assign  <- attr(x, "assign")
     xnames          <- colnames(x)
     if(is.null( xnames ))
-         xnames <- paste("x", 1:p, sep = "")
-
+        xnames <- paste("x", 1:p, sep = "")
+    
     qrx <- list()
     qrx[[1]]  <- qr( x * sqrt(w[,1]) )
-
+    
     pivot <- qrx[[1]]$pivot
     rank  <- qrx[[1]]$rank
     p1    <- 1:rank
-
+    
     if (rank < p & !singular.ok ) 
-    stop("singular fit encountered") 
-	
+        stop("singular fit encountered") 
+    
     x.ind <- x[,pivot[p1], drop=FALSE]
-	
+    
     coefficients  <- matrix(nrow=p,ncol=ny)
     txy      <-  t( x.ind ) %*% ( y*w )
-     chol2invqrx  <-  list()
-	
-	hat.X  <- txX <- list()
-	
-	for( i in 1:(ncol(w)) ) {
-	qrx[[i]]         <- qr( x * sqrt(w[,i]) )
-	chol2invqrx[[i]] <- chol2inv( qrx[[i]]$qr[p1,p1, drop=FALSE] )
-	coefficients[ pivot[p1], ] <-  chol2invqrx[[i]] %*% txy
-	txX[[i]] <- t(x.ind*w[,i]) %*% x.ind
-	}
-	
-	dimnames(coefficients) <- list(xnames,dimnames(y)[[2]])
-	
-	
-	fitted.values <- x %*% coefficients
-	
-	residuals     <- y - fitted.values
-	# Ensure the numerical stability for weights=0 cases.
-	residuals[ok] <- y[ok]
-	colnames(residuals) <- colnames(y)
-	
-	df.residual   <- n.ok - rank
-	
-	################## END Fit multivariate linear model ##################
-	
-	results$coefficients        <- coefficients
-	results$residuals           <- residuals
-	results$fitted.values       <- fitted.values
-	results$weights             <- w
-	results$rank                <- rank
-	results$qr                  <- qrx # QR decomposition of x * sqrt(w)
-	results$df.residual         <- df.residual
-	
-	return(results)
+    chol2invqrx  <-  list()
+    
+    hat.X  <- txX <- list()
+    
+    for( i in 1:(ncol(w)) ) {
+        qrx[[i]]         <- qr( x * sqrt(w[,i]) )
+        chol2invqrx[[i]] <- chol2inv( qrx[[i]]$qr[p1,p1, drop=FALSE] )
+        coefficients[ pivot[p1], ] <-  chol2invqrx[[i]] %*% txy
+        txX[[i]] <- t(x.ind*w[,i]) %*% x.ind
+    }
+    
+    dimnames(coefficients) <- list(xnames,dimnames(y)[[2]])
+    
+    
+    fitted.values <- x %*% coefficients
+    
+    residuals     <- y - fitted.values
+    # Ensure the numerical stability for weights=0 cases.
+    residuals[ok] <- y[ok]
+    colnames(residuals) <- colnames(y)
+    
+    df.residual   <- n.ok - rank
+    
+    ################## END Fit multivariate linear model ##################
+    
+    results$coefficients        <- coefficients
+    results$residuals           <- residuals
+    results$fitted.values       <- fitted.values
+    results$weights             <- w
+    results$rank                <- rank
+    results$qr                  <- qrx # QR decomposition of x * sqrt(w)
+    results$df.residual         <- df.residual
+    
+    return(results)
 }
